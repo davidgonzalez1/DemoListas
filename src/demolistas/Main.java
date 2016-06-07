b09443e0b59a89e28e50793980473299d8ce0ee3
jorgedavid kugonzalez <jorgedavid kugonzalez>
2016-06-07 14:33:17 -0600

    demolistas java

---
+++ src/demolistas/Main.java
@@ -0,0 +1,19 @@
+package demolistas;
+
+import gui.MainForm;
+
+/**
+ *
+ * @author ramon
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//    MainForm windows = new MainForm();
+//    windows.setVisible(true);
+    }
+    
+}
